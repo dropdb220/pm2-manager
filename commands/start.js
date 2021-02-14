b3542fc75b5aaac46aa0b68dfa48eef3f9e1c587
mswgen <54498184+mswgen@users.noreply.github.com>
2021-02-14 10:14:32 +0900

    리스트 간단히 변경

--- commands/start.js
+++ commands/start.js
@@ -23,7 +23,7 @@ module.exports = {
                 .setFooter(message.author.tag, message.author.displayAvatarURL())
                 .setTimestamp();
             for (let x of lists) {
-                embed.addField(x.name, `상태: ${statuses[x.pm2_env.status]}\nID: ${x.pm_id}\n네임스페이스: ${x.pm2_env.namespace}\n프로세스 버전: ${x.pm2_env.version}\n실행 모드: ${x.pm2_env.exec_mode}\n프로세스 ID(PID): ${x.pid}\n업타임: ${Math.floor((new Date() - x.pm2_env.created_at) / 360000) / 10}시간\n재시작 횟수: ${x.pm2_env.restart_time}회\nCPU 사용량: ${x.monit.cpu}%\nRAM 사용량: ${x.monit.memory / 1048576|0}MB\n사용자: ${x.pm2_env.username}`, true);
+                embed.addField(x.name, `상태: ${statuses[x.pm2_env.status]}\nID: ${x.pm_id}\n실행 모드: ${x.pm2_env.exec_mode}\n프로세스 ID(PID): ${x.pid}\n업타임: ${Math.floor((new Date() - x.pm2_env.created_at) / 360000) / 10}시간\n재시작 횟수: ${x.pm2_env.restart_time}회\nCPU 사용량: ${x.monit.cpu}%\nRAM 사용량: ${x.monit.memory / 1048576|0}MB`, true);
             }
             message.channel.send(embed);
         } catch (e) {
